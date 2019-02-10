3295cf65dd80019fc79d9f0288bdf148451af5d0
appcicd <mb@belai.us>
2019-02-10 14:21:18 -0800

    updated client pages & titles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -11,10 +10,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">Guide</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
